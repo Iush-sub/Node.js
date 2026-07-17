66775471f4944b2dd0e0b9fe755c7f28a1c64e20
unknown <pas082bct004@wrc.edu.np>
2026-07-17 16:20:46 +0545

    conditions,variables and datatypes

--- hello.js
+++ hello.js
@@ -1,1 +1,33 @@
 console.log("hello world")
+// this is the way we do comment for better visualitaiton of codes.
+// also my first program 
+
+// variables in js
+let name="green tea";
+var name2="green tea";
+const name3 = "green tea";
+name=10
+
+console.log(name)
+console.log(name3)
+console.log("name",name)
+
+// conditons in js
+b=10
+b==10
+if(true){
+    console.log("nice try")
+}
+// note if i do name==10 then we giet boolean value in return 
+
+// global and local variable
+
+function green(){
+    const name="isuh";
+    console.log("name in function green ", name)
+}
+green()
+
+
+// ternary operator
+// == check value. === value plus check datatype
