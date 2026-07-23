01fd3c2197c991f992a71b24808b358c2e403109
unknown <pas082bct004@wrc.edu.np>
2026-07-23 18:34:20 +0545

    social profile

---
+++ Assignments/Assignment_0.0.03.js
@@ -0,0 +1,38 @@
+
+
+
+//Object created
+const social=
+{
+    "Username":"Iush Sub",
+    "mail":"xyz@gmail.com",
+    "likes":100,
+    "Comments":[
+
+    ],
+    "friends":[
+
+    ]
+
+}
+
+function welcome(){
+    console.log(" ***Welcome to Profile of Iush The Great.*** \n")
+    console.log("Name: "+ social.Username+"\n")
+    console.log("Mail: "+ social.mail+"\n")
+    console.log("likes: "+ social.likes+"\n")
+    console.log("Coments: "+ social.Comments+"\n")
+    console.log("Friends: "+ social.friends+"\n")
+
+}
+welcome()
+
+
+
+
+
+function likes(){
+    social.likes++;
+}
+
+
